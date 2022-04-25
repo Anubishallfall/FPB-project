5a46badba820313f3c54437436347ad14276148c
Mauro Peixoto <maurograp24@gmail.com>
2022-04-25 16:45:28 -0300

    primeira versão api projeto FPB

---
+++ src/main/java/com/example/fpbtech/FpbtechApplication.java
@@ -0,0 +1,13 @@
+package com.example.fpbtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpbtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpbtechApplication.class, args);
+	}
+
+}
